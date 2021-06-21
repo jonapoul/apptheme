3cc5a2fdbd65d092ec3a2c70c967a4c448af509c
jonapoul <jpoulton@protonmail.com>
2021-06-21 21:55:43 +0100

    Linting

--- library/src/main/java/com/jonapoul/theme/TypedArrayUtils.kt
+++ library/src/main/java/com/jonapoul/theme/TypedArrayUtils.kt
@@ -12,4 +12,3 @@ internal fun getAttr(context: Context, attr: Int, fallbackAttr: Int): Int {
     context.theme.resolveAttribute(attr, value, true)
     return if (value.resourceId != 0) attr else fallbackAttr
 }
-
